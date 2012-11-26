e17e38ec8a70095789002900e1724e826cdc6a92
Victor Lloret <victor.anduril@gmail.com>
2012-11-26 13:54:11 +0100

    Destroy in cascade for nested models done

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,16 @@
 class Patient < ActiveRecord::Base
+  	#attributes from nested models (family and medical records) accepted by patients form
   	attr_accessible :admission, :birth_date, :cap, :cap_doctor, :cip, :code, :dni, :exit, :exitus, :image, :insurance_comp, :name, :registered, :room, :sex, :family_attributes, :medical_record_attributes
-	has_one :family
-	has_one :medical_record
-	accepts_nested_attributes_for :family, :allow_destroy => true
+	
+	#relation one-to-one in patients with family and medical records
+	#destroy in cascade enabled (when you destroy a patient you destroy his family and medical record) 
+	has_one :family, :dependent => :destroy
+	has_one :medical_record, :dependent => :destroy
+	
+	#attributes from nested models (family and medical records) accepted by patients form
+  	accepts_nested_attributes_for :family, :allow_destroy => true
 	accepts_nested_attributes_for :medical_record, :allow_destroy => true
+	
 	belongs_to :user
-
-	#validates_presence_of :name => "no puede ser blanco"
 
 end
